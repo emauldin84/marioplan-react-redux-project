2a8e4df8428eea99ad1da5bd0e84c921ca6eb80b
Eric Mauldin <emauldin84@gmail.com>
2019-05-07 22:05:19 -0400

    adds project details component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
+class App extends React.Component{
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className=''>
-        <Navbar />
-        <Switch>
-          <Route path='/' component={ Dashboard } />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  );
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={ Dashboard } />
+            <Route path='/project/:id' component={ ProjectDetails } />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
