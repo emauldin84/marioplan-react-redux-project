ef4877c931daf47c53c33ee2355f16ddcaf38601
Eric Mauldin <emauldin84@gmail.com>
2019-05-09 21:49:06 -0400

    install redux and react-redux and adds s store with reducers

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+import { connect } from 'react-redux'
 
 class Dashboard extends React.Component {
     render(){
+        // console.log(this.props)
+        const { projects } = this.props;
         return(
             <div className='dashboard container'>
                 <div className='row'>
 
                     <div className='col s12 m6'>
-                        <ProjectList />
+                        <ProjectList projects={projects}/>
                     </div>
                     <div className='col s12 m5 offset-m1'>
                         <Notifications />
@@ -21,5 +24,10 @@ class Dashboard extends React.Component {
     }
 }
 
+const mapStateToProps = (state) => {
+    return{
+        projects: state.project.projects
+    }
+}
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)
---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,25 @@
+const initState = {
+    projects: [
+        {
+            id: '1', 
+            title: 'help me find Pluto', 
+            ontent: 'blah blah blah'
+        },
+        {
+            id: '2', 
+            title: 'collect all the stars', 
+            content: 'blah blah blah'
+        },
+        {
+            id: '3', 
+            title: 'blast off in 3..2..', 
+            content: 'blah blah blah'
+        }
+    ]
+}
+
+const projectReducer = (state=initState, action) => {
+    return state
+}
+
+export default projectReducer;
